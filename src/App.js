f0b3a272f1648d8fd7a026a611040f4d1b5710f0
abdelrazek <abdelrazekhdd@gmail.com>
2022-01-25 00:03:02 +0200

    initial styling and background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="bg-yellow-800">Hello World!</div>;
+  return (
+    <div className="font-spartan container mx-auto bg-light-grayish-cyan">
+      <header className="bg-mobile md:bg-desktop bg-dark-cyan bg-no-repeat bg-cover h-24"></header>
+      <main></main>
+    </div>
+  );
 }
 
 export default App;
